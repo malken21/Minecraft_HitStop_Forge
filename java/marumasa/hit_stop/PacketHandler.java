e7b265d55f35c7c0df4275829fd6cf8054059e3b
malken21 <Malken_Marumasa@outlook.jp>
2022-08-26 20:23:07 +0900

    Add files via upload

---
+++ java/marumasa/hit_stop/PacketHandler.java
@@ -0,0 +1,9 @@
+package marumasa.hit_stop;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.network.NetworkDirection;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.simple.SimpleChannel;
+
+public class PacketHandler {
+}
