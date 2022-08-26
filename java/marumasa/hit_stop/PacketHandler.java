94f70bc69c12904ab997b41a41249bbaa250af7e
malken21 <Malken_Marumasa@outlook.jp>
2022-08-26 20:23:21 +0900

    Delete PacketHandler.java

--- java/marumasa/hit_stop/PacketHandler.java
+++
@@ -1,9 +0,0 @@
-package marumasa.hit_stop;
-
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.network.NetworkDirection;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.simple.SimpleChannel;
-
-public class PacketHandler {
-}
